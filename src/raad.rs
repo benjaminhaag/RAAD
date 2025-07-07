9aeff3e85fd21e5d9fb6eae54bc962b77b9ed62b
Benjamin Haag <me@benjaminhaag.it>
2025-07-07 21:39:16 +0200

    modules and vec playground

---
+++ src/raad.rs
@@ -0,0 +1,19 @@
+
+#[derive(Debug)]
+pub enum Option<T> {
+    None,
+    Some(T),
+}
+
+impl<T> Option<T> {
+    pub fn unwrap(self) -> T {
+        self.expect("called `Option::unwrap()` on a `None` value")
+    }
+
+    pub fn expect(self, error: &str) -> T {
+        match self {
+            Option::Some(v) => v,
+            Option::None => panic!("{}", error),
+        }
+    }
+}
